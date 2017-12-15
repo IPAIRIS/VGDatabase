da4583afabcd1d69fd03582df15ead1835ea6e42
Brett <blparis@wisc.edu>
2017-12-15 16:21:53 -0600

    - updated dataservice api paths

--- VGDatabase/src/app/data.service.ts
+++ VGDatabase/src/app/data.service.ts
@@ -15,51 +15,51 @@ export class DataService {
   	// Games
 
   	getGamesFromGID(gid: string) {
-		return this.http.get("http://localhost:8080/api/games/gid/" + gid).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/games/gid/" + gid).map(result => this.result = result.json().data);
 	}
 
   	getGamesFromTitle(title: string) {
-		return this.http.get("http://localhost:8080/api/games/title/" + title).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/games/title/" + title).map(result => this.result = result.json().data);
 	}
 
   	getGamesFromDeveloperName(dName: string) {
-		return this.http.get("http://localhost:8080/api/games/developerName/" + dName).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/games/developerName/" + dName).map(result => this.result = result.json().data);
 	}
 
   	getAllGames() {
-		return this.http.get("http://localhost:8080/api/games/all").map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/games/all").map(result => this.result = result.json().data);
 	}	
 
 	insertGame(game: any) {
-		return this.hClient.post('http://localhost:8080/api/games/insert', game).subscribe();
+		return this.hClient.post('http://104.198.184.180/api/games/insert', game).subscribe();
 	}	
 
 	// Users
 
   	getUsersFromUID(uid: string) {
-		return this.http.get("http://localhost:8080/api/users/uid/" + uid).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/users/uid/" + uid).map(result => this.result = result.json().data);
 	}
 
   	getUsersFromEmail(email: string) {
-		return this.http.get("http://localhost:8080/api/users/email/" + email).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/users/email/" + email).map(result => this.result = result.json().data);
 	}
 
   	getAllUsers() {
-		return this.http.get("http://localhost:8080/api/users/all").map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/users/all").map(result => this.result = result.json().data);
 	}	
 
   	getUsersFromLastName(lName: string) {
-		return this.http.get("http://localhost:8080/api/users/lastName/" + lName).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/users/lastName/" + lName).map(result => this.result = result.json().data);
 	}
 
 	// Players	
 
   	getPlayersFromUID(uid: string) {
-		return this.http.get("http://localhost:8080/api/players/uid/" + uid).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/players/uid/" + uid).map(result => this.result = result.json().data);
 	}
 
   	getPlayersFromUsername(username: string) {
-		return this.http.get("http://localhost:8080/api/players/username/" + username).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/players/username/" + username).map(result => this.result = result.json().data);
 	}
 
   	getPlayerFromEmail(email: string) {
@@ -67,128 +67,128 @@ export class DataService {
 	}
 
   	getAllPlayers() {
-		return this.http.get("http://localhost:8080/api/players/all").map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/players/all").map(result => this.result = result.json().data);
 	}	
 
 	insertPlayer(player: any) {
-		return this.hClient.post('http://localhost:8080/api/players/insert', player).subscribe();
+		return this.hClient.post('http://104.198.184.180/api/players/insert', player).subscribe();
 	}		
 
 	// Developers
 
   	getDevelopersFromUID(uid: string) {
-		return this.http.get("http://localhost:8080/api/developers/uid/" + uid).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/developers/uid/" + uid).map(result => this.result = result.json().data);
 	}
 
   	getDevelopersFromName(name: string) {
-		return this.http.get("http://localhost:8080/api/developers/name/" + name).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/developers/name/" + name).map(result => this.result = result.json().data);
 	}
 
   	getDeveloperFromEmail(email: string) {
-		return this.http.get("http://localhost:8080/api/developers/email/" + email).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/developers/email/" + email).map(result => this.result = result.json().data);
 	}	
 
   	getAllDevelopers() {
-		return this.http.get("http://localhost:8080/api/developers/all").map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/developers/all").map(result => this.result = result.json().data);
 	}		
 
 	insertDeveloper(developer: any) {
-		return this.hClient.post('http://localhost:8080/api/developers/insert', developer).subscribe();
+		return this.hClient.post('http://104.198.184.180/api/developers/insert', developer).subscribe();
 	}		
 
 	// Moderators
 
   	getModeratorsFromUID(uid: string) {
-		return this.http.get("http://localhost:8080/api/moderators/uid/" + uid).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/moderators/uid/" + uid).map(result => this.result = result.json().data);
 	}
 
   	getModeratorsFromGID(gid: string) {
-		return this.http.get("http://localhost:8080/api/moderators/gid/" + gid).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/moderators/gid/" + gid).map(result => this.result = result.json().data);
 	}
 
   	getModeratorFromEmail(email: string) {
-		return this.http.get("http://localhost:8080/api/moderators/email/" + email).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/moderators/email/" + email).map(result => this.result = result.json().data);
 	}
 
   	getAllModerators() {
-		return this.http.get("http://localhost:8080/api/moderators/all").map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/moderators/all").map(result => this.result = result.json().data);
 	}	
 
 	insertModerator(moderator: any) {
-		return this.hClient.post('http://localhost:8080/api/moderators/insert', moderator).subscribe();
+		return this.hClient.post('http://104.198.184.180/api/moderators/insert', moderator).subscribe();
 	}		
 
 	// Reviews
 
   	getReviewsFromUID(uid: string) {
-		return this.http.get("http://localhost:8080/api/reviews/uid/" + uid).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/reviews/uid/" + uid).map(result => this.result = result.json().data);
 	}
 
   	getReviewsFromUsername(username: string) {
-		return this.http.get("http://localhost:8080/api/reviews/username/" + username).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/reviews/username/" + username).map(result => this.result = result.json().data);
 	}
 
   	getReviewsFromGID(gid: string) {
-		return this.http.get("http://localhost:8080/api/reviews/gid/" + gid).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/reviews/gid/" + gid).map(result => this.result = result.json().data);
 	}
 
   	getReviewsFromGameTitle(gTitle: string) {
-		return this.http.get("http://localhost:8080/api/reviews/gameTitle/" + gTitle).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/reviews/gameTitle/" + gTitle).map(result => this.result = result.json().data);
 	}
 
   	getAllReviews() {
-		return this.http.get("http://localhost:8080/api/reviews/all").map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/reviews/all").map(result => this.result = result.json().data);
 	}		
 
 	insertReview(review: any) {
-		return this.hClient.post('http://localhost:8080/api/reviews/insert', review).subscribe();
+		return this.hClient.post('http://104.198.184.180/api/reviews/insert', review).subscribe();
 	}	
 
 	// Purchases
 
   	getPurchasesFromUID(uid: string) {
-		return this.http.get("http://localhost:8080/api/purchases/uid/" + uid).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/purchases/uid/" + uid).map(result => this.result = result.json().data);
 	}
 
   	getPurchasesFromUsername(username: string) {
-		return this.http.get("http://localhost:8080/api/purchases/username/" + username).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/purchases/username/" + username).map(result => this.result = result.json().data);
 	}
 
   	getPurchasesFromGID(gid: string) {
-		return this.http.get("http://localhost:8080/api/purchases/gid/" + gid).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/purchases/gid/" + gid).map(result => this.result = result.json().data);
 	}
 
   	getPurchasesFromGameTitle(gTitle: string) {
-		return this.http.get("http://localhost:8080/api/purchases/gameTitle/" + gTitle).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/purchases/gameTitle/" + gTitle).map(result => this.result = result.json().data);
 	}
 
   	getAllPurchases() {
-		return this.http.get("http://localhost:8080/api/purchases/all").map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/purchases/all").map(result => this.result = result.json().data);
 	}	
 
 	insertPurchase(purchase: any) {
-		return this.hClient.post('http://localhost:8080/api/purchases/insert', purchase).subscribe();
+		return this.hClient.post('http://104.198.184.180/api/purchases/insert', purchase).subscribe();
 	}		
 
 	// Achievements
 
   	getAchievementsFromGID(gid: string) {
-		return this.http.get("http://localhost:8080/api/achievements/gid/" + gid).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/achievements/gid/" + gid).map(result => this.result = result.json().data);
 	}
 
   	getAchievementsFromGameTitle(gTitle: string) {
-		return this.http.get("http://localhost:8080/api/achievements/gameTitle/" + gTitle).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/achievements/gameTitle/" + gTitle).map(result => this.result = result.json().data);
 	}
 
   	getAchievement(gid: string, number: string) {
-		return this.http.get("http://localhost:8080/api/achievements/key/" + gid + "/" + number).map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/achievements/key/" + gid + "/" + number).map(result => this.result = result.json().data);
 	}	
 
   	getAllAchievements() {
-		return this.http.get("http://localhost:8080/api/achievements/all").map(result => this.result = result.json().data);
+		return this.http.get("http://104.198.184.180/api/achievements/all").map(result => this.result = result.json().data);
 	}			
 
 	insertAchievement(achievement: any) {
-		return this.hClient.post('http://localhost:8080/api/achievements/insert', achievement).subscribe();
+		return this.hClient.post('http://104.198.184.180/api/achievements/insert', achievement).subscribe();
 	}		
 }
